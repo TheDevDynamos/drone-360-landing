3a0c90c97c59b9adce11db020e6dbaa7e79a358b
Enam <mdenamahmedchowdhury@gmail.com>
2023-06-05 11:05:50 +0600

    Add Bottstrap

--- src/sections/hero/Hero.jsx
+++ src/sections/hero/Hero.jsx
@@ -3,7 +3,13 @@ import React from "react";
 import "./hero.css";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="">
+      Hero
+      <h1>this is hero section</h1>
+      <button className="btn btn-primary">Check Bootstrap</button>
+    </div>
+  );
 };
 
 export default Hero;
